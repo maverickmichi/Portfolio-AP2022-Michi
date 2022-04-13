c51b9104484a02684e3dfbc36a12f0d3b5e8adb1
Michi <35858687+maverickmichi@users.noreply.github.com>
2022-04-13 12:36:12 -0300

    Commit de viaje
    Para laburar tranquila estando afuera. La verdad no hay grandes cambios aca salvo que estoy cocinando la base de datos y creo un detalle por corregir con un boton de edit en el frontend

---
+++ Frontend/src/script.js
@@ -0,0 +1,34 @@
+function cambiarTitulo()
+{
+    let tituloInput=prompt("Escribí una nuevo nombre");
+    document.getElementById("tituloBoton").innerHTML=tituloInput;
+
+}
+
+function cambiarSubtitulo()
+{
+    let subtituloInput=prompt("Escribí una nueva descripción");
+    document.getElementById("subtituloBoton").innerHTML=subtituloInput;
+
+}
+
+function cambiarConocimientos()
+{
+    let conocimientosInput=prompt("Escribí otro set de conocimientos");
+    document.getElementById("conocimientoBoton").innerHTML=conocimientosInput;
+
+}
+
+function cambiarEducación()
+{
+    let educacionInput=prompt("Escribí nuevos lugares de estudio");
+    document.getElementById("educacionBoton").innerHTML=educacionInput;
+
+}
+
+function cambiarSkills()
+{
+    let educacionInput=prompt("Escribí nuevas habilidades");
+    document.getElementById("skillsBoton").innerHTML=skillsInput;
+
+}
