6ae243d844193800cc71ea7fa272d3406fff572a
Michi <35858687+maverickmichi@users.noreply.github.com>
2022-04-25 16:20:23 -0300

    Edit button fix

---
+++ Main/src/main.java
@@ -0,0 +1,14 @@
+public class main {
+ 
+    public static void main(String[] args) {
+        Persona jose = new Persona(0, "", "Juanestrella", "00001");
+        jose.setNombre("Jose");
+        jose.saludar();
+            
+        Persona ana = new Persona(1, "Ana", "Bolica", "37351");
+        ana.saludar();
+        
+    }
+     
+       
+}
